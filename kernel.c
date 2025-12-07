94ca5ce36728bc6dc7fb2d6b2bdaca64487b2d7d
eldahl <71466904+eldahl@users.noreply.github.com>
2025-12-07 21:50:19 +0100

    fade out, fade in

--- kernel.c
+++ kernel.c
@@ -27,32 +27,49 @@ void kmain(void) {
         for (;;) { __asm__ volatile ("hlt"); }
     }
 
-    // Draw a simple gradient / bars to prove linear framebuffer is mapped.
-    for (uint32_t y = 0; y < info->fb_height; ++y) {
-        uint8_t* row = (uint8_t*)(uintptr_t)(info->fb_addr + y * info->fb_pitch);
-        for (uint32_t x = 0; x < info->fb_width; ++x) {
-            // Create a gradient across X/Y and a moving bar.
-            uint8_t r = (x * 255) / (info->fb_width ? info->fb_width : 1);
-            uint8_t g = (y * 255) / (info->fb_height ? info->fb_height : 1);
-            uint8_t b = ((x + y) * 255) / ((info->fb_width + info->fb_height) ? (info->fb_width + info->fb_height) : 1);
+		int fadeOut = 1;
+		int count = 0;
 
-            if (info->fb_bpp == 32) {
-                uint32_t* p32 = (uint32_t*)row;
-                p32[x] = ((uint32_t)r << 16) | ((uint32_t)g << 8) | b;
-            } else if (info->fb_bpp == 24) {
-                uint8_t* p = row + x * 3;
-                p[0] = b;
-                p[1] = g;
-                p[2] = r;
-            } else if (info->fb_bpp == 16) {
-                uint16_t* p16 = (uint16_t*)row;
-                uint16_t rv = ((uint16_t)(r >> 3) << 11) |
-                              ((uint16_t)(g >> 2) << 5)  |
-                              ((uint16_t)(b >> 3));
-                p16[x] = rv;
-            }
-        }
-    }
+		for(;;) {
+			// Draw a simple gradient / bars to prove linear framebuffer is mapped.
+			for (uint32_t y = 0; y < info->fb_height; ++y) {
+					uint8_t* row = (uint8_t*)(uintptr_t)(info->fb_addr + y * info->fb_pitch);
+					for (uint32_t x = 0; x < info->fb_width; ++x) {
+							// Create a gradient across X/Y and a moving bar.
+							uint8_t r = (x * (255 - count)) / (info->fb_width ? info->fb_width : 1);
+							uint8_t g = (y * (255 - count)) / (info->fb_height ? info->fb_height : 1);
+							uint8_t b = ((x + y) * (255 - count)) / ((info->fb_width + info->fb_height) ? (info->fb_width + info->fb_height) : 1);
+
+							if (info->fb_bpp == 32) {
+									uint32_t* p32 = (uint32_t*)row;
+									p32[x] = ((uint32_t)r << 16) | ((uint32_t)g << 8) | b;
+							} else if (info->fb_bpp == 24) {
+									uint8_t* p = row + x * 3;
+									p[0] = r;
+									p[1] = g;
+									p[2] = r;
+							} else if (info->fb_bpp == 16) {
+									uint16_t* p16 = (uint16_t*)row;
+									uint16_t rv = ((uint16_t)(r >> 3) << 11) |
+																((uint16_t)(g >> 2) << 5)  |
+																((uint16_t)(b >> 3));
+									p16[x] = rv;
+							}
+					}
+			}
+			if(count == 255){
+				fadeOut = 0;
+			}
+			else if(count == 0) {
+				fadeOut = 1;
+			}
+
+			if(fadeOut == 1)
+				count++;
+			else
+				count--;
+		}
+
 
     for (;;) {
         __asm__ volatile ("hlt");
